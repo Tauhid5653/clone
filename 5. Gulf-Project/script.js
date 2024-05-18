567d62f256f026246f164491fe73a74ad26b9be6
Tauhid Alam <tauhid5653@gmail.com>
2024-05-18 16:09:25 +0530

    commit

---
+++ 5. Gulf-Project/script.js
@@ -0,0 +1,99 @@
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function(dets){
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
+    blur.style.left = dets.x -112 + "px"
+    blur.style.top = dets.y -112 + "px"
+})
+
+var h4all = document.querySelectorAll(".nav h4");
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        crsr.style.scale = 3
+        crsr.style.border = "1px solid #fff"
+        crsr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave", function(){
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #fff"
+        crsr.style.backgroundColor = "#95c11e"
+    })
+})
+
+
+
+
+
+gsap.to(".nav",{
+    backgroundColor : "#000",
+    height : "80",
+    duration : 0.1,
+    scrollTrigger: {
+        trigger : ".nav",
+        scroller: "body",
+        start : "top -10%",
+        end : "top -11%",
+        scrub : 1,
+    }
+})
+
+gsap.to(".main", {
+    backgroundColor : "#000",
+    scrollTrigger : {
+        trigger :".main",
+        scroller : "body",
+        start : "top -25%",
+        end : "top -70%",
+        scrub:2
+    }
+});
+
+gsap.from("#about-us img, #about-us-in", {
+    y:50,
+    opacity : 0,
+    duration:1,
+    scrollTrigger:{
+        trigger: "#about-us",
+        scroller: "body",
+        start: "top 60%",
+        end: "end 55%",
+        scrub : 3
+    }
+})
+
+gsap.from("#colon1",{
+    y : 70,
+    x : -70,
+    scrollTrigger : {
+        trigger: "#colon1",
+        scroller : "body",
+        start : "top 55%",
+        end : "top 45%",
+        scrub : 1
+    }
+})
+gsap.from("#colon2",{
+    y : 70,
+    x : 70,
+    scrollTrigger : {
+        trigger: "#colon1",
+        scroller : "body",
+        start : "top 55%",
+        end : "top 45%",
+        scrub : 4
+    }
+})
+
+gsap.from("#page4 h1",{
+    y: 50,
+    scrollTrigger:{
+        trigger: "#page4 h1",
+        scroller: "body",
+        start : "top 75%",
+        end : "top 70%",
+        scrub : 4
+    }
+})
+
